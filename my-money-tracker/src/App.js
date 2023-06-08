37b6088efdd76fb64e83092b71c5f896b46c1ffd
Lara Mernik <lara.mernik@student.um.si>
2023-06-08 04:08:48 +0200

    Design in manjši popravki na FrontEndu

--- my-money-tracker/src/App.js
+++ my-money-tracker/src/App.js
@@ -1,4 +1,4 @@
-import {Link, Route, Routes} from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Home from './pages/Home';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -8,64 +8,72 @@ import { useState } from 'react';
 import Overview from './pages/Overview';
 import AllTransactions from './pages/AllTransactions';
 import Budget from './pages/Budget';
-
+import logo from './images/MoneyTracker_logo.png' // relative path to image 
 
 function App() {
   const [showLoginModal, setShowLoginModal] = useState(false)
 
   return (
     <>
-    <header className='fixed-header'>
-          <ul className="nav nav-pills">
-              <li className="nav-item">
-              <button className="Navbtn"><Link to='/pages/Home'>Home</Link></button>
-              </li>
-              <li className="nav-item">
-              <button className="Navbtn"><Link to='/pages/Overview'>Overview</Link></button>
-            </li>
-            <li className="nav-item">
-              <button className="Navbtn"><Link to='/pages/Budget'>Budgeting</Link></button>
-            </li>
-            <li className="nav-item">
-              <button className="Navbtn"><Link to='/pages/AllTransactions'>Transactions</Link></button>
-            </li>
-            <li>
+      <div className='fixed-header'>
+        <img className='logo-navbar' src={logo} />
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <Link to='/pages/Home'>
+              <button className="Navbtn">Home</button>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/pages/Overview'><button className="Navbtn">Overview</button></Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/pages/Budget'><button className="Navbtn">Budgeting</button></Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/pages/AllTransactions'><button className="Navbtn">Transactions</button></Link>
+          </li>
+          <li>
             <button className="Navbtn" onClick={() => setShowLoginModal(true)}>Login</button>
-              <Modal show={showLoginModal} onHide={() => setShowLoginModal(false)}>
-                  <Form>
-                      <Modal.Header closeButton>
-                      <Modal.Title>Log In</Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body>
-                      <Form.Group className="mb-3" controlId="Username">
-                          <Form.Label>Username:</Form.Label>
-                          <Form.Control type="text" required />
-                      </Form.Group>
-                      <Form.Group className="mb-3" controlId="Paswword">
-                          <Form.Label>Password:</Form.Label>
-                          <Form.Control
-                            type="password" required
-                          />
-                      </Form.Group>
-                      <div className="d-flex justify-content-end">
-                          <Button variant="primary" type="submit">
-                          Login
-                          </Button>
-                      </div>
-                      </Modal.Body>
-                  </Form>
-              </Modal>
-            </li>
-          </ul>
-          </header>
+            <Modal show={showLoginModal} onHide={() => setShowLoginModal(false)}>
+              <Form>
+                <Modal.Header closeButton>
+                  <Modal.Title>Log In</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form.Group className="mb-3" controlId="Username">
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control type="text" required />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="Paswword">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                      type="password" required
+                    />
+                  </Form.Group>
+                  <div className="d-flex justify-content-end">
+                    <Button variant="primary" type="submit">
+                      Login
+                    </Button>
+                  </div>
+                </Modal.Body>
+              </Form>
+            </Modal>
+          </li>
+        </ul>
+      </div>
       <Routes>
-      <Route path="/pages/Home" element={<Home />} />
-      <Route path="/pages/Overview" element={<Overview />} />
-      <Route path="/pages/AllTransactions" element={<AllTransactions />} />
-      <Route path="/pages/Budget" element={<Budget />} />
+
+        <Route path="/pages/Home" element={<Home />} />
+        <Route path="/pages/Overview" element={<Overview />} />
+        <Route path="/pages/AllTransactions" element={<AllTransactions />} />
+        <Route path="/pages/Budget" element={<Budget />} />
       </Routes>
-      <footer className='footer'>FooterContent</footer>
-  </>
+
+      <footer className='footer fixed-footer'>
+        <div className='footer-line'></div>
+        Money Tracker
+      </footer>
+    </>
   );
 }
 
--- my-money-tracker/src/pages/Home.js
+++ my-money-tracker/src/pages/Home.js
@@ -4,17 +4,25 @@ import { Form, Modal, Button } from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../App.css'
 import CategoryTransactions from '../components/CategoryTransactions'
+import logo from './MoneyTracker_logo.png' // relative path to image 
 
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <div>
-            <h2>Home Page</h2>
+
             <div className="MainHome">
-            <div className="card"><div className="HomeContainer">Company Details</div></div>
-            <div className="card"><div className="HomeContainer">Contact Information</div></div>
-            <div className="card"><div className="HomeContainer">Getting started guide link</div></div>
+            <div className="card"><div className="HomeContainer"><h1>New to MoneyTracker?</h1>
+                    <p><h2>Some quick tips:</h2></p>
+                    <ul>
+                    <p> <li><b>OVERVIEW:</b> A place where all your finances are - You can see your transactions (positive and negative ones), spent and remaing money.</li>
+                    <li><b>BUDGETING</b>: Divide your expenses in categories and track exactly where your money goes!</li>
+                    <li><b>TRANSACTIONS:</b> This is a page for you to add your transactions to track them!</li>
+                    </p>
+                    </ul>
+                    </div></div>
+                </div>
+                
             </div>
-        </div>
     )
 
 
